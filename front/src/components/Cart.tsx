2a26271e2ae214f4f9f0832e08c29c5884eab3f4
Piotr <sixtyto@gmail.com>
2021-04-02 21:06:42 +0200

    refactor cart component

--- front/src/components/Cart.tsx
+++ front/src/components/Cart.tsx
@@ -9,12 +9,13 @@ import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 
+import { getPrice } from "../utils";
+
 const Cart: React.FC = () => {
   const dispatch = useAppDispatch();
   const removeFromCart = (id: number) => {
     dispatch(removeFromCartDispatcher(id));
   };
-  const getPrice = (price: number) => (price / 100).toFixed(2);
   const {
     books,
     cart: { order },
@@ -23,15 +24,15 @@ const Cart: React.FC = () => {
   const cartSummary = getPrice(
     order
       .map(
-        (item) =>
-          item.quantity * books.filter((order) => order.id === item.id)[0].price
+        ({ id, quantity }) =>
+          quantity * books.filter((book) => book.id === id)[0].price
       )
       .reduce((a, b) => a + b, 0)
   );
   return (
     <Col>
       <h2>Koszyk</h2>
-      {order.length > 0 ? (
+      {order.length ? (
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -42,23 +43,18 @@ const Cart: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {order.map((order) => (
-              <tr key={order.id}>
-                <td>{order.id}</td>
-                <td>{books.filter((item) => item.id === order.id)[0].title}</td>
+            {order.map(({ id, quantity }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{books.filter((item) => item.id === id)[0].title}</td>
                 <td className="d-flex justify-content-between">
-                  {order.quantity}
-                  <Button
-                    variant="danger"
-                    onClick={() => removeFromCart(order.id)}
-                  >
+                  {quantity}
+                  <Button variant="danger" onClick={() => removeFromCart(id)}>
                     Usuń
                   </Button>
                 </td>
                 <td>
-                  {getPrice(
-                    books.filter((item) => item.id === order.id)[0].price
-                  )}
+                  {getPrice(books.filter((item) => item.id === id)[0].price)}
                 </td>
               </tr>
             ))}
@@ -73,7 +69,7 @@ const Cart: React.FC = () => {
       ) : (
         <p>Koszyk jest pusty</p>
       )}
-      {order.length > 0 ? (
+      {order.length ? (
         <Row>
           <Col xs={{ offset: 10 }}>
             <Link to="/order" className="btn btn-primary">
